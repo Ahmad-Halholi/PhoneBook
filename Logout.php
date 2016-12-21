c422c6e1c33b13b56bd2c0d784519474b59e34d6
Ahmad-Halholi <ahmad.halholi1995@gmail.com>
2016-12-21 11:37:27 +0200

    Update Logout.php

--- Logout.php
+++ Logout.php
@@ -1,13 +1,15 @@
 <?php
 session_start();
-if(isset($_SESSION['id']))
+if(isset($_SESSION['id'])) /* if the user is not holding a session and directly accessed the link he will be redirected */
     {
     unset($_SESSION['id']);
+    unset($_SESSION['is_admin']);
     session_destroy();
     include "config.php";
     header('Location: '.'http://'.$_SERVER['SERVER_NAME'].$scriptfolder.'index.php?error=3');
     }
     else {
+	    /* just in case of direct entry */
 echo "<script>document.documentElement.innerHTML=\"<h3>You shall not access this page unless you are logged in , your attempt was logged !!</h3>\";</script>";
 die("<br><h1>logout direct - logged to the server ~!</h1><br>");
 }
