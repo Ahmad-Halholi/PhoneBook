f9d9287496ee52bbc540849e7e9e2579e35f30d3
Ahmad-Halholi <ahmad.halholi1995@gmail.com>
2016-12-21 11:15:18 +0200

    Add files via upload

--- resourses/styles/index.php
+++ resourses/styles/index.php
@@ -1,1 +1,6 @@
-
+<?
+session_start();
+include "../../config.php";
+$index='http://'.$_SERVER['SERVER_NAME'].$scriptfolder.'index.php';
+echo "Are you lost ? here ,"."<a href=$index>"." back to index"."</a>";
+?>
